2794a48b5d54ff705bcdf613e9985374058a26db
robertistok <robertistok94@gmail.com>
2019-06-10 20:31:46 +0200

    Auth + upser user

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,5 +1,38 @@
 const { forwardTo } = require("prisma-binding");
 
-const Mutation = {};
+const wunderlist = require("../lib/wunderlist");
+
+const Mutation = {
+  upsertUser: forwardTo("db"),
+
+  authUserWithWunderlist: async (parent, args, ctx, info) => {
+    const { code } = args;
+
+    const { accessToken } = await wunderlist.request.post.oauth({ code });
+    const { data: currentUser } = await wunderlist.request.get.user();
+
+    const loggedInUser = await ctx.db.mutation.upsertUser(
+      {
+        where: { email: currentUser.email },
+        create: {
+          email: currentUser.email,
+          wunderlistAccessToken: accessToken,
+          revision: currentUser.revision,
+          name: currentUser.name
+        },
+        update: {
+          wunderlistAccessToken: accessToken,
+          revision: currentUser.revision,
+          name: currentUser.name
+        }
+      },
+      info
+    );
+
+    console.log(loggedInUser);
+
+    // wunderlistAPIInstanceForAuth.http;
+  }
+};
 
 module.exports = Mutation;
