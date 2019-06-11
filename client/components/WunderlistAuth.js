2c01f7ca505658b68fe1418a86f39b31af4c7ce3
robertistok <robertistok94@gmail.com>
2019-06-11 08:16:45 +0200

    extract wl auth into separate file

---
+++ client/components/WunderlistAuth.js
@@ -0,0 +1,15 @@
+import { WUNDERLIST_CLIENT_ID, WUNDERLIST_REDIRECT_URI, WUNDERLIST_RANDOM } from "../config";
+
+const WunderlistAuth = () => {
+  return (
+    <div>
+      <a
+        href={`https://www.wunderlist.com/oauth/authorize?client_id=${WUNDERLIST_CLIENT_ID}&redirect_uri=${WUNDERLIST_REDIRECT_URI}&state=${WUNDERLIST_RANDOM}`}
+      >
+        Sign in with Wunderlist
+      </a>
+    </div>
+  );
+};
+
+export default WunderlistAuth;
