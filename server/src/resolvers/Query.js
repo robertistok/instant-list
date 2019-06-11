bc4f8611cd9833d18dbbf50b0842c257b0d4a889
robertistok <robertistok94@gmail.com>
2019-06-11 08:18:20 +0200

    get current logged in user query

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,5 +1,15 @@
 const { forwardTo } = require("prisma-binding");
 
-const Query = {};
+const Query = {
+  me: async (parent, args, ctx, info) => {
+    const { userId } = ctx.request;
+
+    if (!userId) {
+      return null;
+    }
+
+    return ctx.db.query.user({ where: { id: userId } });
+  }
+};
 
 module.exports = Query;
