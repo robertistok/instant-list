274a027e68c4875e180aea90e11bf8abe0d7220e
robertistok <robertistok94@gmail.com>
2019-06-11 08:18:39 +0200

    set token when signed up/logged in

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,37 +1,36 @@
 const { forwardTo } = require("prisma-binding");
 
 const wunderlist = require("../lib/wunderlist");
+const { setToken } = require("../lib/jwt");
 
 const Mutation = {
   upsertUser: forwardTo("db"),
 
+  // eslint-disable-next-line no-unused-vars
   authUserWithWunderlist: async (parent, args, ctx, info) => {
     const { code } = args;
 
     const { accessToken } = await wunderlist.request.post.oauth({ code });
     const { data: currentUser } = await wunderlist.request.get.user();
 
-    const loggedInUser = await ctx.db.mutation.upsertUser(
-      {
-        where: { email: currentUser.email },
-        create: {
-          email: currentUser.email,
-          wunderlistAccessToken: accessToken,
-          revision: currentUser.revision,
-          name: currentUser.name
-        },
-        update: {
-          wunderlistAccessToken: accessToken,
-          revision: currentUser.revision,
-          name: currentUser.name
-        }
+    const user = await ctx.db.mutation.upsertUser({
+      where: { email: currentUser.email },
+      create: {
+        email: currentUser.email,
+        wunderlistAccessToken: accessToken,
+        revision: currentUser.revision,
+        name: currentUser.name
       },
-      info
-    );
+      update: {
+        wunderlistAccessToken: accessToken,
+        revision: currentUser.revision,
+        name: currentUser.name
+      }
+    });
 
-    console.log(loggedInUser);
+    setToken({ data: { userId: user.id }, res: ctx.response });
 
-    // wunderlistAPIInstanceForAuth.http;
+    return user;
   }
 };
 
