8c39a2dc756f2670563b76c2dcb8da602c8f06d2
robertistok <robertistok94@gmail.com>
2019-06-13 09:05:33 +0200

    Make it nicer

--- client/components/WunderlistAuth.js
+++ client/components/WunderlistAuth.js
@@ -1,4 +1,5 @@
-import { Button } from "antd";
+import { Button } from "grommet";
+import { List } from "grommet-icons";
 
 import { WUNDERLIST_CLIENT_ID, WUNDERLIST_REDIRECT_URI, WUNDERLIST_RANDOM } from "../config";
 
@@ -6,12 +7,12 @@ const WunderlistAuth = () => {
   return (
     <div>
       <Button
-        type="primary"
-        icon="unordered-list"
+        primary
+        reverse
+        label="Sign in with Wunderlist"
+        icon={<List />}
         href={`https://www.wunderlist.com/oauth/authorize?client_id=${WUNDERLIST_CLIENT_ID}&redirect_uri=${WUNDERLIST_REDIRECT_URI}&state=${WUNDERLIST_RANDOM}`}
-      >
-        Sign in with Wunderlist
-      </Button>
+      />
     </div>
   );
 };
