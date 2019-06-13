9b95ff4641e5aab7e2940620e88a501af57e03c8
robertistok <robertistok94@gmail.com>
2019-06-13 09:04:46 +0200

    refetch user query if logged in

--- client/pages/wunderlist-auth.js
+++ client/pages/wunderlist-auth.js
@@ -3,6 +3,8 @@ import Router, { withRouter } from "next/router";
 import { useMutation } from "react-apollo-hooks";
 import gql from "graphql-tag";
 
+import { CURRENT_USER_QUERY } from "../hooks/user";
+
 const AUTH_USER_WITH_WUNDERLIST_MUTATION = gql`
   mutation AUTH_USER_WITH_WUNDERLIST_MUTATION($code: String) {
     authUserWithWunderlist(code: $code) {
@@ -15,8 +17,11 @@ const WunderlistAuth = ({ router }) => {
   const authUserWithWunderlist = useMutation(AUTH_USER_WITH_WUNDERLIST_MUTATION);
 
   useEffect(() => {
-    authUserWithWunderlist({ variables: { code: router.query.code } });
-    Router.push("/");
+    authUserWithWunderlist({
+      variables: { code: router.query.code },
+      refetchQueries: [{ query: CURRENT_USER_QUERY }],
+      update: () => Router.push("/")
+    });
   }, []);
 
   // co;
