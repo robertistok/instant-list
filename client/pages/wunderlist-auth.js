531a2009ce28b9c6966219c2398953adfb51d943
robertistok <robertistok94@gmail.com>
2019-06-14 08:34:41 +0200

    update wunderlist-auth to reflect the user hook export changes

--- client/pages/wunderlist-auth.js
+++ client/pages/wunderlist-auth.js
@@ -3,30 +3,29 @@ import Router, { withRouter } from "next/router";
 import { useMutation } from "react-apollo-hooks";
 import gql from "graphql-tag";
 
-import { CURRENT_USER_QUERY } from "../hooks/user";
+import { queries } from "../hooks/user";
 
-const AUTH_USER_WITH_WUNDERLIST_MUTATION = gql`
-  mutation AUTH_USER_WITH_WUNDERLIST_MUTATION($code: String) {
-    authUserWithWunderlist(code: $code) {
+const AUTH_WITH_WUNDERLIST_MUTATION = gql`
+  mutation AUTH_WITH_WUNDERLIST_MUTATION($code: String) {
+    authWithWunderlist(code: $code) {
       id
     }
   }
 `;
 
 const WunderlistAuth = ({ router }) => {
-  const authUserWithWunderlist = useMutation(AUTH_USER_WITH_WUNDERLIST_MUTATION);
+  const authWithWunderlist = useMutation(AUTH_WITH_WUNDERLIST_MUTATION);
 
   useEffect(() => {
-    authUserWithWunderlist({
+    authWithWunderlist({
       variables: { code: router.query.code },
-      refetchQueries: [{ query: CURRENT_USER_QUERY }],
+      refetchQueries: [{ query: queries.CURRENT_USER_QUERY }],
+      awaitRefetchQueries: true,
       update: () => Router.push("/")
     });
   }, []);
 
-  // co;
-
-  return <div>WL auth</div>;
+  return <div>Logging you in securely with Wunderlist, hold on..</div>;
 };
 
 export default withRouter(WunderlistAuth);
