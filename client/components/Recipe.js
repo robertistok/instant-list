c22dde9e9da842397767c910c2a959d79c08978e
robertistok <robertistok94@gmail.com>
2019-06-24 20:42:09 +0200

    Add animation to the list items

--- client/components/Recipe.js
+++ client/components/Recipe.js
@@ -1,10 +1,13 @@
 import { withRouter } from "next/router";
 import { useQuery } from "react-apollo-hooks";
+import { animated } from "react-spring";
 import gql from "graphql-tag";
 import { Box, Button, Heading, Text } from "grommet";
 import { Add } from "grommet-icons";
 
 import Loader from "./common/Loader";
+
+import { useListAnimation } from "../hooks/animations";
 
 const RECIPE_QUERY = gql`
   query RECIPE_QUERY($where: RecipeWhereUniqueInput!) {
@@ -29,15 +32,19 @@ const RECIPE_QUERY = gql`
 `;
 
 const Recipe = ({ router }) => {
-  const { data, loading } = useQuery(RECIPE_QUERY, {
+  const {
+    data: { recipe },
+    loading
+  } = useQuery(RECIPE_QUERY, {
     variables: { where: { id: router.query.id } }
   });
+
+  const ingredientsTrail = useListAnimation(recipe && recipe.ingredients.length);
+  const stepsTrail = useListAnimation(recipe && recipe.steps.length);
 
   if (loading) {
     return <Loader />;
   }
-
-  const { recipe } = data;
 
   return (
     <div>
@@ -45,18 +52,21 @@ const Recipe = ({ router }) => {
       <Text>{recipe.description}</Text>
       <Heading level="3">Steps</Heading>
       <Box as="ul" gap="medium">
-        {recipe.steps.map((step, index) => (
-          <Box align="start" direction="row" gap="small">
-            <Heading className="info" level="4" margin="0">
-              {`${index + 1}.`}
-            </Heading>
-            <Text key={step}>{step}</Text>
-          </Box>
-        ))}
+        {stepsTrail.map((style, index) => {
+          const step = recipe.steps[index];
+          return (
+            <AnimatedBox align="start" direction="row" gap="small" key={step} style={style}>
+              <Heading className="info" level="4" margin="0">
+                {`${index + 1}.`}
+              </Heading>
+              <Text key={step}>{step}</Text>
+            </AnimatedBox>
+          );
+        })}
       </Box>
 
       <Heading level="3">Ingredients</Heading>
-      <Box gap="medium">
+      {/* <Box gap="medium">
         {recipe.ingredients.map(ingredient => (
           <Box align="center" as="li" direction="row" gap="small">
             <Button hoverIndicator icon={<Add />} plain />
@@ -68,9 +78,27 @@ const Recipe = ({ router }) => {
             </Text>
           </Box>
         ))}
+      </Box> */}
+
+      <Box gap="medium">
+        {ingredientsTrail.map((props, index) => {
+          const { id, ingredient, measurementUnit, quantity } = recipe.ingredients[index];
+
+          return (
+            <AnimatedBox align="center" as="li" direction="row" gap="small" style={props} key={id}>
+              <Button hoverIndicator icon={<Add />} plain />
+              <Text key={id}>
+                {`${quantity || ""} ${(measurementUnit && measurementUnit.toLowerCase()) || ""}
+                    ${ingredient.name}`}
+              </Text>
+            </AnimatedBox>
+          );
+        })}
       </Box>
     </div>
   );
 };
+
+const AnimatedBox = animated(Box);
 
 export default withRouter(Recipe);
