734ab8b80759abd9b474f145be2f0a0175913e08
robertistok <robertistok94@gmail.com>
2019-06-25 22:01:57 +0200

    Add delete/edit buttons
    No action when clicked on them yet though

--- client/components/Recipe.js
+++ client/components/Recipe.js
@@ -1,4 +1,4 @@
-import { withRouter } from "next/router";
+/* eslint-disable no-alert */
 import { useQuery } from "react-apollo-hooks";
 import { animated } from "react-spring";
 import gql from "graphql-tag";
@@ -46,41 +46,45 @@ const Recipe = ({ router }) => {
     return <Loader />;
   }
 
+  const handleDelete = () => {
+    const confirmedResponse = window.confirm("Are you sure you want to delete this recipe?");
+
+    console.log(confirmedResponse);
+  };
+
   return (
     <div>
       <Heading level="2">{recipe.title}</Heading>
-      <Text>{recipe.description}</Text>
+
+      <Text a11yTitle="Description">{recipe.description}</Text>
+
       <Heading level="3">Steps</Heading>
-      <Box as="ul" gap="medium">
+      <Box a11yTitle="Steps container" as="ul" gap="medium">
         {stepsTrail.map((style, index) => {
           const step = recipe.steps[index];
+          const stepNumber = index + 1;
           return (
-            <AnimatedBox align="start" direction="row" gap="small" key={step} style={style}>
+            <AnimatedBox
+              a11yTitle={`Step number ${stepNumber}`}
+              align="start"
+              direction="row"
+              gap="small"
+              key={step}
+              style={style}
+            >
               <Heading className="info" level="4" margin="0">
                 {`${index + 1}.`}
               </Heading>
-              <Text key={step}>{step}</Text>
+              <Text a11yTitle={`Description for step number ${stepNumber}`} key={step}>
+                {step}
+              </Text>
             </AnimatedBox>
           );
         })}
       </Box>
 
       <Heading level="3">Ingredients</Heading>
-      {/* <Box gap="medium">
-        {recipe.ingredients.map(ingredient => (
-          <Box align="center" as="li" direction="row" gap="small">
-            <Button hoverIndicator icon={<Add />} plain />
-            <Text key={ingredient.id}>
-              {`${ingredient.quantity || ""} ${(ingredient.measurementUnit &&
-                ingredient.measurementUnit.toLowerCase()) ||
-                ""}
-                    ${ingredient.ingredient.name}`}
-            </Text>
-          </Box>
-        ))}
-      </Box> */}
-
-      <Box gap="medium">
+      <Box a11yTitle="Ingredients container" gap="medium">
         {ingredientsTrail.map((props, index) => {
           const { id, ingredient, measurementUnit, quantity } = recipe.ingredients[index];
 
@@ -95,10 +99,15 @@ const Recipe = ({ router }) => {
           );
         })}
       </Box>
+
+      <Box a11yTitle="Recipe action buttons" direction="row" gap="medium" margin="40px 0px">
+        <Button label="Edit" primary />
+        <Button color="wunderlistRed" label="Delete" onClick={handleDelete} primary />
+      </Box>
     </div>
   );
 };
 
 const AnimatedBox = animated(Box);
 
-export default withRouter(Recipe);
+export default Recipe;
