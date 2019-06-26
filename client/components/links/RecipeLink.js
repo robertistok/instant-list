c62c5187bcf1cf7cd953099e48e6ea5d3dceab19
robertistok <robertistok94@gmail.com>
2019-06-26 21:52:52 +0200

    Dynamic recipe link component

--- client/components/links/RecipeLink.js
+++ client/components/links/RecipeLink.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import styled from "styled-components";
 
-const RecipeLink = ({ id, children }) => (
-  <Link href={`/recipe?id=${id}`} as={`/recipe/${id}`} passHref>
+const RecipeLink = ({ path = "/recipe", id, children }) => (
+  <Link href={`${path}?id=${id}`} as={`${path}/${id}`} passHref>
     <StyledLink>{children}</StyledLink>
   </Link>
 );
