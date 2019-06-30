3e6a446a24fdd7960361bd686f342370c8cf0405
robertistok <robertistok94@gmail.com>
2019-06-30 18:29:52 +0200

    Resolve the bug of wrong item display when delete
    The name did not update properly

--- client/components/UpsertRecipe/StepCard.js
+++ client/components/UpsertRecipe/StepCard.js
@@ -1,74 +1,89 @@
+import React, { useEffect, useState } from "react";
+import nanoid from "nanoid";
 import styled from "styled-components";
 import { Box, Button, FormField, Grid, Heading, ResponsiveContext, TextArea } from "grommet";
 import { Add, Trash } from "grommet-icons";
 
-const StepCard = ({
-  addStep,
-  deleteStep,
-  description,
-  handleInputChange,
-  index,
-  isFirstStep,
-  isLastStep
-}) => {
-  const stepNumber = index + 1;
+import { usePrevious } from "../../hooks/utils";
 
-  return (
-    <ResponsiveContext.Consumer>
-      {size => {
-        const isMedium = ["medium", "small"].includes(size);
+const StepCard = React.memo(
+  ({
+    addStep,
+    deleteStep,
+    description,
+    handleInputChange,
+    isFirstStep,
+    isLastStep,
+    stepNumber,
+    totalSteps
+  }) => {
+    const [stepId, setStepId] = useState(stepNumber);
+    const prevTotalSteps = usePrevious(totalSteps);
+    useEffect(() => setStepId(nanoid()), [prevTotalSteps > totalSteps]);
 
-        return (
-          <StyledGrid a11yTitle={`Step number ${stepNumber}`} gap="small">
-            <Heading className="info" level="4" margin="small">
-              {`${stepNumber}.`}
-            </Heading>
+    return (
+      <ResponsiveContext.Consumer>
+        {size => {
+          const isMedium = ["medium", "small"].includes(size);
 
-            <FormField
-              a11yTitle={`Write the description for step number ${stepNumber}`}
-              component={TextArea}
-              name={`step-${index}`}
-              onChange={handleInputChange}
-              placeholder={`Write step ${stepNumber}`}
-              required
-              resize="vertical"
-              value={description}
-            />
+          return (
+            <StyledGrid a11yTitle={`Step number ${stepNumber}`} gap="small">
+              <Heading className="info" level="4" margin="small">
+                {`${stepNumber}.`}
+              </Heading>
 
-            <Box
-              a11yTitle="Delete/add new ingedient button"
-              align="center"
-              className="action-buttons"
-              direction="row"
-              justify="end"
-              gap="small"
-            >
-              {isLastStep && (
-                <Button
-                  a11yTitle="Add new step button"
-                  hoverIndicator
-                  icon={<Add />}
-                  label={isMedium ? "" : "Next"}
-                  onClick={addStep}
-                />
-              )}
+              <FormField
+                a11yTitle={`Write the description for step number ${stepNumber}`}
+                component={TextArea}
+                name={`step-${stepId}`}
+                onChange={handleInputChange}
+                placeholder={`Write step ${stepNumber}`}
+                required
+                resize="vertical"
+                value={description}
+              />
 
-              {!isFirstStep && (
-                <Button
-                  a11yTitle={`Delete step ${stepNumber} button`}
-                  hoverIndicator
-                  icon={<Trash />}
-                  label={isMedium ? "" : "Delete"}
-                  onClick={deleteStep}
-                />
-              )}
-            </Box>
-          </StyledGrid>
-        );
-      }}
-    </ResponsiveContext.Consumer>
-  );
-};
+              <Box
+                a11yTitle="Delete/add new ingedient button"
+                align="center"
+                className="action-buttons"
+                direction="row"
+                justify="end"
+                gap="small"
+              >
+                {isLastStep && (
+                  <Button
+                    a11yTitle="Add new step button"
+                    hoverIndicator
+                    icon={<Add />}
+                    label={isMedium ? "" : "Next"}
+                    onClick={addStep}
+                  />
+                )}
+
+                {!isFirstStep && (
+                  <Button
+                    a11yTitle={`Delete step ${stepNumber} button`}
+                    hoverIndicator
+                    icon={<Trash />}
+                    label={isMedium ? "" : "Delete"}
+                    onClick={deleteStep}
+                  />
+                )}
+              </Box>
+            </StyledGrid>
+          );
+        }}
+      </ResponsiveContext.Consumer>
+    );
+  },
+  (prevProps, nextProps) => {
+    return (
+      prevProps.totalSteps === nextProps.totalSteps &&
+      prevProps.description === nextProps.description
+    );
+  }
+);
 
 const StyledGrid = styled(Grid)`
   grid-template-columns: 2% 59% auto;
