8844f510db7525c7cd1ad4a5693909e4ceb073e6
robertistok <robertistok94@gmail.com>
2019-07-03 08:16:52 +0200

    Fetch users project from todoist

---
+++ client/pages/settings.js
@@ -0,0 +1,12 @@
+import Settings from "../components/Settings";
+import PleaseSignIn from "../components/PleaseSignIn";
+
+const SettingsPage = () => {
+  return (
+    <PleaseSignIn>
+      <Settings />
+    </PleaseSignIn>
+  );
+};
+
+export default SettingsPage;
