ab181e13234cd8946660b3d49e16d5680801d304
robertistok <robertistok94@gmail.com>
2019-07-08 19:51:17 +0200

    Use useRef instead of useState(React.createRef() + cleanup

--- client/components/common/Tooltip.js
+++ client/components/common/Tooltip.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import { Box, Drop } from "grommet";
 
 const Tooltip = ({ children, label, align = { top: "bottom" } }) => {
-  const [ref] = useState(React.createRef());
+  const ref = useRef(null);
   const [over, setOver] = useState(false);
 
   const onMouseEnter = () => setOver(true);
@@ -14,7 +14,7 @@ const Tooltip = ({ children, label, align = { top: "bottom" } }) => {
       {children({ ref, onMouseEnter, onMouseLeave })}
       {ref.current && over && (
         <Drop align={align} target={ref.current} plain>
-          <Box margin="xsmall" pad="small">
+          <Box margin="xsmall" pad="small" background={label ? "dark-3" : "inherit"}>
             {label}
           </Box>
         </Drop>
