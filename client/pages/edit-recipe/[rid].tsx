81f6acd2dfdf3cc70b4bb5e99460fd77dd3e9beb
robertistok <robertistok94@gmail.com>
2019-07-11 08:11:58 +0200

    Edit recipe to typescript

--- client/pages/edit-recipe/[rid].tsx
+++ client/pages/edit-recipe/[rid].tsx
@@ -1,11 +1,13 @@
+import { BaseRouter } from "next-server/dist/lib/router/router";
 import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
 
 import EditRecipe from "../../components/EditRecipe";
 import PleaseSignIn from "../../components/PleaseSignIn";
 
-const EditRecipePage = () => {
-  const router = useRouter();
-  const { rid } = router.query;
+const EditRecipePage: React.FunctionComponent<Props> = () => {
+  const router: BaseRouter = useRouter();
+  const { rid }: ParsedUrlQuery = router.query;
 
   return (
     <PleaseSignIn>
