9c1e031de647e6eb48109c7d284c74b7886a0745
robertistok <robertistok94@gmail.com>
2019-07-11 08:12:09 +0200

    Recipe to typescript

---
+++ client/pages/recipe/[rid].tsx
@@ -0,0 +1,21 @@
+import { BaseRouter } from "next-server/dist/lib/router/router";
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+
+import PleaseSignIn from "../../components/PleaseSignIn";
+import Recipe from "../../components/Recipe/Recipe";
+
+interface Props {}
+
+const RecipePage: React.FunctionComponent<Props> = () => {
+  const router: BaseRouter = useRouter();
+  const { rid }: ParsedUrlQuery = router.query;
+
+  return (
+    <PleaseSignIn>
+      <Recipe id={rid} />
+    </PleaseSignIn>
+  );
+};
+
+export default RecipePage;
