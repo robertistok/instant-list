16256be6299e776b7e0f5389722fd52b1600507c
robertistok <robertistok94@gmail.com>
2019-10-03 10:15:51 +0300

    Add prisma secret

--- server/src/db.js
+++ server/src/db.js
@@ -1,12 +1,12 @@
 // This file connects to the remote prisma DB and gives us the ability to query it with JS
 const { Prisma } = require("prisma-binding");
 
-const { PRISMA_ENDPOINT } = require("./config");
+const { PRISMA_ENDPOINT, PRISMA_SECRET } = require("./config");
 
 const db = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   endpoint: PRISMA_ENDPOINT,
-  // secret: process.env.PRISMA_SECRET,
+  secret: PRISMA_SECRET,
   debug: false
 });
 
