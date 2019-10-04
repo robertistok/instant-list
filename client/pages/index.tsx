4c76dd1eb6fc569881f64378792bf2bc9793b2cd
robertistok <robertistok94@gmail.com>
2019-10-04 12:25:07 +0300

    If no shopping list, redirect user to select one

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,21 @@
+import Router, { useRouter } from "next/router";
+
 import PleaseSignIn from "../components/PleaseSignIn";
 import Recipes from "../components/Recipes";
 
+import useUser from "../hooks/user";
+
 interface Props {}
 
-const Home: React.FunctionComponent<Props> = () => {
+const Home: React.FunctionComponent<Props> = (): React.ReactElement => {
+  const { data } = useUser();
+
+  if (data.me && !data.me.shoppingList) {
+    alert("You must select a shopping list before continuing");
+    Router.push("/settings");
+    return null;
+  }
+
   return (
     <PleaseSignIn>
       <Recipes />
